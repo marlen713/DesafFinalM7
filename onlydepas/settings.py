85a26e745a194717b7ad3430f65014a0724ab4f7
Marlen <ma000161@gmail.com>
2024-05-17 12:23:11 -0400

    agregando modificacion en settings

--- onlydepas/settings.py
+++ onlydepas/settings.py
@@ -28,9 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
-#CSRF_TRUSTED_ORIGINS = [mi url]
+ALLOWED_HOSTS = ['*', 'http://desaffinalm7.onrender.com','desaffinalm7.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['http://desaffinalm7.onrender.com']
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'   #ojo uso esto para que si no estoy logueada lapagina me redicione alli
